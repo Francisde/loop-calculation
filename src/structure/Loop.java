092f2cf33838823566bb1812cef4cf39ddc78704
Martin <martin.mehlhose@gmx.de>
2019-06-30 15:11:15 +0200

    complete Loop.java

--- src/structure/Loop.java
+++ src/structure/Loop.java
@@ -2,7 +2,7 @@ package structure;
 
 import java.util.List;
 
-/*
+/**
  * This class defines a loop statement. A loop statement has the form
  * LOOP(xi){
  * P;
@@ -14,5 +14,32 @@ public class Loop extends Statement {
     private int i;
     private List<Statement> statements;
 
+    /**
+     * the public constructor for the Loop object
+     *
+     * @param i          the index of the count value
+     * @param statements a list of the inner statements in the loop
+     */
+    public Loop(int i, List<Statement> statements) {
+        this.i = i;
+        this.statements = statements;
+    }
 
+    /**
+     * public getter for the index value
+     *
+     * @return index value i as an integer
+     */
+    public int getI() {
+        return i;
+    }
+
+    /**
+     * public getter for the list of inner statements
+     *
+     * @return a list of Statement objects as a List object
+     */
+    public List<Statement> getStatements() {
+        return statements;
+    }
 }
