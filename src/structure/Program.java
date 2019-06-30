f7bfe4241f69c143e72e06b28f15125c410669a7
Martin <martin.mehlhose@gmx.de>
2019-06-30 21:23:21 +0200

    update Javadoc to Program.java

--- src/structure/Program.java
+++ src/structure/Program.java
@@ -7,10 +7,19 @@ public class Program {
 
     private List<Statement> statementList;
 
+    /**
+     * the public constructor for the Program object
+     *
+     * @param statementList the statementlist as a List object
+     */
     public Program(List<Statement> statementList) {
         this.statementList = statementList;
     }
 
+    /**
+     * the public getter for the statementList
+     * @return all Statement objects as a list
+     */
     public List<Statement> getStatementList() {
         return statementList;
     }
