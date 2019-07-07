8691ccd2e3f707d063273171f577310c7df7dff9
Martin <martin.mehlhose@gmx.de>
2019-07-08 00:45:21 +0200

    implement all methods to read and parse assignments

--- src/IO/parseLoopFile.java
+++
@@ -1,7 +0,0 @@
-package IO;
-
-/**
- * This class implements methods which can parse a .loop file in an Program object
- */
-public class parseLoopFile {
-}
--- src/parser/ParseAssignment.java
+++ src/parser/ParseAssignment.java
@@ -5,7 +5,38 @@ import structure.Assignment;
 public class ParseAssignment {
 
     public static Assignment parseAssignment(String input) {
+        //remove possible ";" at the end of the String
+        input = input.replace(";", "");
+        input = input.strip();
+        input = input.toLowerCase();
+        String[] bothSides = input.split("=");
+        if (bothSides.length != 2) {
+            // throw exception
+        }
+        String leftSide = bothSides[0].replace("x", "");
+        leftSide = leftSide.strip();
+        int i = Integer.parseInt(leftSide);
 
-        return new Assignment(1, 2, 2);
+        // change + to @ because split cant check '+' character
+        bothSides[1] = bothSides[1].replace("+", "@");
+        String[] rightSide = bothSides[1].split("@");
+        if (rightSide.length != 2) {
+            // throw exception
+        }
+        String left = rightSide[0].replace("x", "");
+        left = left.strip();
+        int j = Integer.parseInt(left);
+
+        String right = rightSide[1].strip();
+        int c;
+        if (right.contains("-")) {
+            right = right.replace("-", "");
+            c = Integer.parseInt(right);
+            c = 0 - c;
+        } else {
+            c = Integer.parseInt(right);
+        }
+
+        return new Assignment(i, j, c);
     }
 }
